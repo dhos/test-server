09613969f078c79db2a85f2d53e49ec548a103e3
Stan Le <stan.le1107@gmail.com>
2017-09-07 12:40:02 -0700

    time

--- seed.js
+++ seed.js
@@ -208,7 +208,7 @@ var organizeData = csvData => {
         var letterToBeCreated = {
             bank: banks[columns[0]],
             date: new Date(columns[1]),
-            expiry: new Date.now(),
+            expiry: new Date(1504813174792),
             contract: columns[2],
             lc_number: columns[3],
             country: countries[columns[4]],
