88986f8f63083273bea441af8e6536e203b77e07
Shivraj S <shivrajs.bayar@gmail.com>
2017-09-19 00:23:02 +0530

    refining and removing unwanted shit

--- browser/js/admin/customerlist/newCustomer.js
+++ browser/js/admin/customerlist/newCustomer.js
@@ -7,12 +7,12 @@ app.config(function($stateProvider) {
     })
 });
 
-app.controller('newCustomerCtrl', function($scope, customerFactory, $state, $rootScope, LETTER_EVENTS, lcFactory, openModal, usSpinnerService) {
+app.controller('newCustomerCtrl', function($scope, customerFactory, $state, $rootScope, LETTER_EVENTS, lcFactory, openModal) {
     $scope.submitting = false
     $scope.makeUser = (user) => {
         openModal('Create Customer', 'Are you sure?', 'prompt', 'confirm').then(result => {
             if(result){
-                
+
                 customerFactory.createCustomer(user).then(user => {
                     $state.go('customerList')
                 })
