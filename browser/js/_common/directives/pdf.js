0634b704c2daf5c620d847f6c9214143d076fb2f
Stan Le <stan.le1107@gmail.com>
2017-09-17 22:53:09 -0700

    fixes

--- browser/js/_common/directives/pdf.js
+++ browser/js/_common/directives/pdf.js
@@ -2,7 +2,7 @@ app.directive('pdf', function() {
     return {
         restrict: 'E',
         link: function(scope, element, attrs) {
-            element.replaceWith(`<object type="application/pdf" data="uploads/${attrs.source}" width="100%" height="70vh">
+            element.replaceWith(`<object type="application/pdf" data="uploads/${attrs.source}" width="100%" height="576px">
                                 <p>Alternative text - include a link <a href="uploads/${attrs.source}">to the PDF!</a></p></object>'`);
         }
     };
