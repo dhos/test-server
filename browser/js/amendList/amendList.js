8f6e16051161f6a88d2571422cc404bcdfd054b2
Stan Le <stan.le1107@gmail.com>
2017-09-21 19:18:40 -0700

    fixes

--- browser/js/amendList/amendList.js
+++ browser/js/amendList/amendList.js
@@ -15,7 +15,7 @@ app.config(function($stateProvider) {
     })
 });
 
-app.controller('amendListCtrl', function($scope, amended, $state, countryFactory, userFactory, bankFactory, lcFactory, LETTER_EVENTS, $rootScope, customerFactory, clientFactory) {
+app.controller('amendListCtrl', function($scope, amended, $state, countryFactory, userFactory, bankFactory, lcFactory, LETTER_EVENTS, $rootScope, customerFactory, clientFactory, lcFactory) {
     //get banks
     $scope.letters = $scope.Amended
     $scope.banks = {}
