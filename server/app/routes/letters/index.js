5a57140347817615cc087e757ce0c505019b54ce
Stan Le <stan.le1107@gmail.com>
2017-10-04 23:16:28 -0700

    fixes

--- server/app/routes/letters/index.js
+++ server/app/routes/letters/index.js
@@ -31,25 +31,6 @@ var ensureAuthenticated = function(req, res, next) {
         next(err);
     }
 };
-// create reusable transporter object using the default SMTP transport
-
-
-// setup email data with unicode symbols
-
-
-// send mail with defined transport object
-// transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//         return console.log(error);
-//     }
-//     console.log('Message sent: %s', info.messageId);
-//     // Preview only available when sending through an Ethereal account
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-// });
-
 
 //fetches
 
